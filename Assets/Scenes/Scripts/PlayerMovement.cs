fde3a3d10276740564611874c64072244e6751c3
Will Nguyen <mdnguyen123321@gmail.com>
2025-07-12 13:58:39 -0400

    Added fences to all sides and WASD movement

--- Assets/Scenes/Scripts/PlayerMovement.cs
+++ Assets/Scenes/Scripts/PlayerMovement.cs
@@ -18,19 +18,17 @@ public class PlayerMovement : MonoBehaviour {
         float movedirx = 0f;
         float movediry = 0f;
 
-        if (Input.GetKey(KeyCode.DownArrow)) {
-            movediry -= 1f;
-        }
-        
-        if (Input.GetKey(KeyCode.UpArrow)) {
+        // Arrow Keys
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W)) {
             movediry += 1f;
         }
-
-        if (Input.GetKey(KeyCode.LeftArrow)) {
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S)) {
+            movediry -= 1f;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)) {
             movedirx -= 1f;
         }
-
-        if (Input.GetKey(KeyCode.RightArrow)) {
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) {
             movedirx += 1f;
         }
 
