54e4b2adcc3b13e6b24a5ead5835ab25b678103d
Will Nguyen <mdnguyen123321@gmail.com>
2025-07-11 22:45:42 -0400

    First initialization: Basic basis of the game with a menu, basic level with drawing mechanics and credit

--- Assets/Scenes/Scripts/OnClicked.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-public class OnClicked : MonoBehaviour
-{
-    public void Clicked(){
-    }
-}
--- Assets/Scenes/Scripts/OnPressed.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class OnPressed : MonoBehaviour
-{
-    public void Pressed() {
-        Debug.Log("THE BUTTON IS PRESSED");
-        FindObjectOfType<GameManager>().CompleteLevel();
-    }
-}
